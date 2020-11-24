1a6e50eb2bcb5c6229fe6761e4fed707d4d167ad
Davyd Souza <davyd.eduardo.souza1@ibm.com>
2020-11-24 12:03:50 -0300

    Added styling to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // Via this method you'll reference to a file
   templateUrl: './app.component.html',
+  // Via this method you'll write the template inline
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+
+  // You can also have multiple css files, an array is used because we can refence multiple css files.
   styleUrls: ['./app.component.css']
+  // Internal example
+  // styles: [`
+  //   h3 {
+  //     color: dodgerblue;
+  //   }
+  // `]
+
 })
 export class AppComponent {
 }
